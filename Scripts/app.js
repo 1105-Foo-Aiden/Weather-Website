c83d0a1974e58a7db075e45140ec301edd7d03db
Aiden Foo <aidenfoo154@gmail.com>
2023-12-04 15:57:10 -0800

    Initial commit/ Day 1

---
+++ Scripts/app.js
@@ -0,0 +1,6 @@
+let searhBar = document.getElementById("searchBar");
+let location = document.getElementById('location');
+let favorites = document.getElementById('favorties');
+let searchBtn = document.getElementById('searchBtn');
+let date = document.getElementById('date')
+let currentTemp = document.getElementById('currentTemp')
