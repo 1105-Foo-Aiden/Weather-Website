3cfe494a8dbe4e346b36cea3479527acd84b5c69
Aiden Foo <aidenfoo154@gmail.com>
2023-12-06 15:37:54 -0800

    day 3

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Recieved the figma from the person who's website I was going to recreate, and tr
 
 Day 2 Notes
 I kept working on the format of the website along with the JavaScript, I was able to populate most of the information needed for the current day, not all of the fetch requests are set up so I will finish them tomorrow, the bottom 5-day forcast is set up the data just needs to populate inside.
+
+Day 3 Notes
+I was able to finish the search bar and give it functionality and was able to get all the apis called and able to populate in the console. I satarted working on the max/min of each day for the 5 day forcast and was able to get the next 5 days of the week and dates able to populate.
--- Scripts/app.js
+++ Scripts/app.js
@@ -12,6 +12,8 @@ let weatherStat = document.getElementById("weatherStat");
 let longitude;
 let latitude;
 let day = new Date();
+let compareDay = new Date()
+
 
 //Forcast Day 1
 let day1Day = document.getElementById("day1Day");
@@ -38,45 +40,85 @@ let day5Day = document.getElementById("day5Day");
 let day5Status = document.getElementById("day5Status");
 let day5HiLow = document.getElementById("day5HighLow");
 
-day = day.toLocaleDateString("default", {
-  weekday: `long`,
-  day: "numeric",
-  month: "long",
-});
+
+
+day1Day.textContent = `${new Date(day.getTime() + 86400000).toLocaleDateString('en-US', {weekday: 'short'})} ${new Date(day.getTime() + 86400000).toLocaleDateString('en-US', {day:'numeric'})}`
+day2Day.textContent = `${new Date(day.getTime() + 86400000 * 2).toLocaleDateString('en-US', {weekday: 'short'})} ${new Date(day.getTime() + 86400000 * 2).toLocaleDateString('en-US', {day:'numeric'})}`
+day3Day.textContent = `${new Date(day.getTime() + 86400000 * 3).toLocaleDateString('en-US', {weekday: 'short'})} ${new Date(day.getTime() + 86400000 * 3).toLocaleDateString('en-US', {day:'numeric'})}`
+day4Day.textContent = `${new Date(day.getTime() + 86400000 * 4).toLocaleDateString('en-US', {weekday: 'short'})} ${new Date(day.getTime() + 86400000 * 4).toLocaleDateString('en-US', {day:'numeric'})}`
+day5Day.textContent = `${new Date(day.getTime() + 86400000 * 5).toLocaleDateString('en-US', {weekday: 'short'})} ${new Date(day.getTime() + 86400000 * 5).toLocaleDateString('en-US', {day:'numeric'})}`
+
+day = day.toLocaleDateString("default", { weekday: `long`, day: "numeric",  month: "short",});
 
 navigator.geolocation.getCurrentPosition(success, errorFunc);
 
 async function success(position) {
-    if(searchBar.value)
+  //Max Arrays
+
+  let day1MaxArr = []
+  let day2MaxArr = []
+  let day3MaxArr = []
+  let day4MaxArr = []
+  let day5MaxArr = []
+
+
+  //Min Arrays
+  let day1MinArr = []
+  let day2MinArr = []
+  let day3MinArr = []
+  let day4MinArr = []
+  let day5MinArr = [] 
+
+  if(searchBar.value)
     {
-        const search = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchBar.value}&limit=1&appid=${apiKey}`);
-        const cityName = await search.json()
-        latitude = cityName.lat;
-        longitude = cityName.lon;
-        console.log(latitude)
+      const search = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchBar.value}&limit=1&appid=${apiKey}`);
+      const cityName = await search.json()
+      console.log(cityName[0].lat)
+      latitude = cityName[0].lat;
+      longitude = cityName[0].lon;
     }
     else
     {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude; 
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude; 
+      const now = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=Imperial`)
+      const currentWeather = await now.json()
+      currentTemp.textContent = `${Math.round(currentWeather.main.temp)}\u00B0`;
+      location.textContent = `${currentWeather.name}`;
+      weatherCondition.textContent = `${currentWeather.weather[0].main}`;
+      weatherStat.src = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`;
     }
 
   const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial&cnt=40`);
 
   const data = await promise.json();
 
-  currentTemp.textContent = `${Math.floor(data.list[0].main.temp)}\u00B0`;
-  location.textContent = `${data.city.name}`;
-  weatherCondition.textContent = `${data.list[0].weather[0].main}`;
-  highLow.textContent = `\uFFEA ${Math.floor(data.list[0].main.temp_max)}\u00B0 \uFFEC ${Math.floor(data.list[0].main.temp_min)}\u00B0`;
-  weatherStat.src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
+  
   for (let i = 0; i < data.list.length; i++) {
-    let dayTime = new Date(data.list[i].dt * 1000);
-    dayTime = dayTime.toLocaleString();
-    console.log(dayTime);
-    
+    let dayTime1 = new Date(data.list[i].dt * 1000);
+    let dayTime2 = new Date(data.list[i].dt * 1000 + 86400000);
+    let dayTime3 = new Date(data.list[i].dt * 1000 + 86400000 * 2);
+    let dayTime4 = new Date(data.list[i].dt * 1000 + 86400000 * 3);
+    let dayTime5 = new Date(data.list[i].dt * 1000 + 86400000 * 4);
+    let dayTime6 = new Date(data.list[i].dt * 1000 + 86400000 * 5);
+
+    if(dayTime1.toLocaleDateString('en-US') === compareDay.toLocaleDateString('en-US')){
+      day1MaxArr.push(data.list[i].main.temp_max);
+      day1MinArr.push(data.list[i].main.temp_min);
+      // console.log(Math.round(Math.floor(...day1MaxArr))) 
+      // console.log(Math.round(Math.floor(...day1MinArr))) 
+    }
   }
   date.textContent = `${day}`;
+  console.log(day1MaxArr)
+  console.log(day1MinArr)
+  console.log(Math.round(Math.max(...day1MaxArr)))
+  console.log(Math.round(Math.min(...day1MinArr)))
+  highLow.innerText = `\uFFEA${Math.round(Math.max(...day1MaxArr))}\u00B0 \uFFEC ${Math.round(Math.min(...day1MinArr))}\u00B0`
+  // currentTemp.textContent = `${Math.round(data.list[0].main.temp)}\u00B0`;
+  // location.textContent = `${data.city.name}`;
+  // weatherCondition.textContent = `${data.list[0].weather[0].main}`;
+  // weatherStat.src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
 
 }
 
@@ -84,8 +126,8 @@ function errorFunc(error) {
   console.log(error.message);
 }
 
-searchBtn.addEventListener('keypress', function(e){
-    searchBar.value = position;
-    success(position)
-    alert()
+searchBtn.addEventListener('click', function(e){
+  console.clear()
+  console.log(searchBar.value)
+  success(searchBar.value)
 });
